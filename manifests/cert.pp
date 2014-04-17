cbf9d44b69790dfc23ded24df7b3bba1c6a0da19
Michael Gruener <michael.gruener@chaosmoon.net>
2014-04-17 14:56:05 +0200

    Add class / type documentation.

--- manifests/cert.pp
+++ manifests/cert.pp
@@ -1,3 +1,118 @@
+# == Define: certtool::cert
+#
+# Manages a ssl certificate and its private / public key. Most parameters
+# correspond to their equivalent in the configuration file used by certtool.
+#
+# === Parameters
+#
+# [*certpath*]
+#   The path where the resulting certificate file should be placed. Default
+#   depends on the distribution being used.
+#
+# [*keypath*]
+#   The path where the private key of the certificate should be placed.
+#   Default depends on the distribution being used.
+#
+# [*pubkeypath*]
+#   The path where the public key of the certificate should be placed. Only
+#   used if $extract_pubkey is true. Default depends on the distribution being
+#   used.
+#
+# [*keybits*]
+#   Number of bits used to create the private key. Default 2048.
+#
+# [*organization*]
+#   The organization (O) of the certificate. Optional
+#   but should be provided the create a working certificate.
+#
+# [*unit*]
+#   The organizational unit (OU) of the certificate. Optional
+#   but should be provided the create a working certificate.
+#
+# [*locality*]
+#   The locality (L) of the certificate. Optional
+#   but should be provided the create a working certificate.
+#
+# [*state*]
+#   The state (ST) of the certificate. Optional
+#   but should be provided the create a working certificate.
+#
+# [*country*]
+#   The country (C) of the certificate. Optional
+#   but should be provided the create a working certificate.
+#
+# [*common_name*]
+#   The common name (CN) of the certificate. Normally the FQDN of system. Optional
+#   but should be provided the create a working certificate.
+#
+# [*serial*]
+#   The serial number of the certificate. Default is a random number.
+#
+# [*expiration_days*]
+#   How long (in days) the certificate should be valid. Default 365.
+#
+# [*uris*]
+#   Array of URIs using this certificate. Optional.
+#
+# [*dns_names*]
+#   Array of alternate names (Subject alternate name) for the certificate. Optional.
+#
+# [*ip*]
+#   Array of IP adresses using this certificate. Optional.
+#
+# [*email*]
+#   E-Mail address of the person which this certificate belongs to. Useful
+#   in client certificates.
+#
+# [*is_ca*]
+#   Set this to true if you want to create a CA certificate. Default false.
+#
+# [*caname*]
+#   The name of the certtool::cert resource of the CA certificate that should sign
+#   this certificate. Only used if this is a non-CA non-self-signed certificate.
+#
+# [*usage*]
+#   Array of usage types for this certificate, examples are
+#   ['tls_www_server', 'signing_key']. See the certtool manpage for more
+#   examples.
+#
+# [*use_request_extensions*]
+#   If the extensions requested in the CSR should be included in the resulting
+#   certificate.
+#
+# [*crl_dist_points*]
+#   Array of URLs that have CRLs (certificate revocation lists). Used in CA
+#   certificates.
+#
+# [*challenge_password*]
+#   Challenge password used in certificate requests
+#
+# [*password*]
+#   Password when encrypting a private key
+#
+# [*crl_number*]
+#
+# [*pkcs12_key_name*]
+#
+# [*extract_pubkey*]
+#   Creates a separate file containing the public key if set to true. Default false.
+#
+# [*combine_keycert*]
+#   If set to true, the private key of the certificate will be included
+#   in the resulting certificate file.
+#
+# [*self_signed*]
+#   If the to true the resulting certificate will be self-signed. CA certifcates
+#   (if $is_ca = true) are always self-signed. Default false.
+#
+# === Author
+# 
+# Michael Gruener <michael.gruener@chaosmoon.net>
+#
+# === Copyright
+#
+# Copyright 2014 Michael Gruener, unless otherwise noted.
+#
 define certtool::cert (
   $certpath = $certtool::params::certpath,
   $keypath = $certtool::params::keypath,
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,29 @@
+# == Class: certtool
+#
+# This class installs the certtool program, used to manage ssl certificates
+#
+# === Parameters
+#
+# [*package*]
+#   Name of the package containing certtool. Default depends on the distribution
+#   being used. 
+#
+# [*ensure*]
+#   Ensure parameter for the package resource. Default "present".
+#
+# === Actions:
+# - Install the package providing certtool 
+#
+# === Requires
+#
+# === Author
+# 
+# Michael Gruener <michael.gruener@chaosmoon.net>
+#
+# === Copyright
+#
+# Copyright 2014 Michael Gruener, unless otherwise noted.
+#
 class certtool (
   $package = $certtool::params::package,
   $ensure = present,
