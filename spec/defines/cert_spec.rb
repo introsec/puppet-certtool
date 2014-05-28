4aa5f1bd7b1d62c3bd5fcfa34389fd5b7437a724
Michael Gruener <michael.gruener@chaosmoon.net>
2014-05-28 16:22:01 +0200

    Add basic tests for CA, self-signed cert creation and public-key extraction

--- spec/defines/cert_spec.rb
+++ spec/defines/cert_spec.rb
@@ -61,6 +61,7 @@ describe 'certtool::cert', :type => :define do
         :pubkeypath => '/rspec/public'
       }
     end
+
     describe "basic requirements" do
       let :params do default_params end
       it { should contain_class("certtool") }
@@ -95,6 +96,36 @@ describe 'certtool::cert', :type => :define do
         :creates => "#{params[:keypath]}/#{title}.key",
         :command => /.*certtool.*--generate-privkey/
       ).that_requires("File[#{params[:keypath]}]") }
+    end
+
+    describe "extracting the pubkey" do
+      let :params do
+        default_params.merge(:extract_pubkey => true)
+      end
+      it { should contain_exec("certtool-pubkey-#{title}").with(
+        :creates => "#{params[:pubkeypath]}/#{title}-pub.key",
+        :command => /.*certtool\s*--load-privkey\s*#{params[:keypath]}\/#{title}.key\s*--pubkey-info\s*--outfile\s*#{params[:pubkeypath]}\/#{title}-pub.key/
+      ).that_requires("Exec[certtool-key-#{title}]") }
+    end
+
+    describe "creating a CA certificate" do
+      let :params do
+        default_params.merge(:is_ca => true)
+      end
+      it { should contain_exec("certtool-ca-#{title}").with(
+        :creates => "#{params[:certpath]}/#{title}.crt",
+        :command => /.*certtool\s*--generate-self-signed\s*--template\s*#{params[:certpath]}\/certtool-#{title}.cfg\s*--load-privkey\s*#{params[:keypath]}\/#{title}.key\s*--outfile\s*#{params[:certpath]}\/#{title}.crt/
+      ).that_requires("File[#{params[:certpath]}/certtool-#{title}.cfg]").that_requires("File[#{params[:keypath]}/#{title}.key]") }
+    end
+
+    describe "creating a self-signed certificate" do
+      let :params do
+        default_params.merge(:self_signed => true)
+      end
+      it { should contain_exec("certtool-cert-#{title}").with(
+        :creates => "#{params[:certpath]}/#{title}.crt",
+        :command => /.*certtool\s*--generate-self-signed\s*--template\s*#{params[:certpath]}\/certtool-#{title}.cfg\s*--load-privkey\s*#{params[:keypath]}\/#{title}.key\s*--outfile\s*#{params[:certpath]}\/#{title}.crt/
+      ).that_requires("File[#{params[:certpath]}/certtool-#{title}.cfg]").that_requires("File[#{params[:keypath]}/#{title}.key]") }
     end
   end
 end
